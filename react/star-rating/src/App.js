dca183b808a9ed9e7ff20c6379fccc4e5ee1d712
ali <ali1995ez@gmail.com>
2022-11-16 23:17:37 +0330

    create star rating

---
+++ react/star-rating/src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+import StarRating from "./components/StarRating";
+function App() {
+  return <StarRating style={{ backgroundColor: "lightblue" }} onDoubleClick={(e) => alert("double click")} />;
+}
+
+export default App;
