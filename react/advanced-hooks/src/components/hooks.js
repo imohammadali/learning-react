d4fb2328145f2fc609b7ba237073d1e5a0311acd
ali <ali1995ez@gmail.com>
2022-11-30 10:01:05 +0330

    add usefetch hook

--- react/advanced-hooks/src/components/hooks.js
+++ react/advanced-hooks/src/components/hooks.js
@@ -54,3 +54,22 @@ export function useMousePosition() {
   }, []);
   return [x, y];
 }
+export function useFetch(uri) {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    if (!uri) return;
+    fetch(uri)
+      .then((data) => data.json())
+      .then(setData)
+      .then(setLoading(false))
+      .catch(setError);
+  }, [uri]);
+
+  return {
+    loading,
+    data,
+    error,
+  };
+}
